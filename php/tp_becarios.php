0f4e92db4c25187f97ad4ecfe8c2011f2d891008
Andrea <Andrea@AndreaGran>
2019-10-01 10:47:15 -0300

    Agrego formulario de solicitud de usuario en la página  de login

--- php/tp_becarios.php
+++ php/tp_becarios.php
@@ -15,7 +15,6 @@
             echo "Convocatoria Becas de Investigaci&oacute;n";
             echo "</div>";
             echo "<div>versi&oacute;n ".toba::proyecto()->get_version();
-            //echo " <a href='ManualModuloDesignaciones.pdf'>Descargar Manual Ayuda</a>" ."</div>";
             //echo " <a href='Disposicion005-15SH.pdf'>Disposici?n</a>" ."</div>";
             //echo " <a href='resol_0442_2017.pdf'>Resoluci&oacute;n</a>" ."</div>";
             echo "</div>";
@@ -100,6 +99,9 @@
 	{
 		echo "</div>";		
 		echo "<div class='login-pie'>";
+                
+                echo "<div> <a href='https://docs.google.com/forms/d/e/1FAIpQLSfnLWF_9tYPsuqAXvtyaFk-e4v--sG6m1u8uLn-vwe2RicdZQ/viewform?usp=sf_link'>Solicitud Usuario</a>" ."</div>";
+                echo "<br>";
                 $anio=date('Y');
 		echo "<div style='font-size:10px;'>Desarrollado por <strong>".toba_recurso::imagen_proyecto('logo_sti_sin.png',true,'30','30')."</strong><br>Equipo de Desarrollo TOBA-UNCOMA<br>Universidad Nacional del Comahue</div>
 			<div >2018 - $anio</div>";
