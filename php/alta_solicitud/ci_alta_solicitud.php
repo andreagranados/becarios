31e1981b5cbf98409a4d153d4acc3f72e85a2526
Andrea <Andrea@AndreaGran>
2021-03-16 12:39:24 -0300

    Impresion Ficha por la UA

--- php/alta_solicitud/ci_alta_solicitud.php
+++ php/alta_solicitud/ci_alta_solicitud.php
@@ -786,7 +786,7 @@ class ci_alta_solicitud extends toba_ci
             //Configuramos el pie de página. El mismo, tendra el número de página centrado en la página y la fecha ubicada a la derecha. 
             //Primero definimos la plantilla para el número de página.
             $formato = utf8_decode('Convocatoria Becas de Investigación (Mocovi)                  '.date('d/m/Y h:i:s a').'     Página {PAGENUM} de {TOTALPAGENUM} ');
-            $pdf->ezStartPageNumbers(400, 20, 8, 'left', $formato, 1); //utf8_d_seguro($formato)
+            $pdf->ezStartPageNumbers(500, 20, 8, 'left', $formato, 1); //utf8_d_seguro($formato)
             
             //Configuración de Título.
             $salida->titulo(utf8_d_seguro(''));    
@@ -1173,7 +1173,7 @@ class ci_alta_solicitud extends toba_ci
             //Configuramos el pie de página. El mismo, tendra el número de página centrado en la página y la fecha ubicada a la derecha. 
             //Primero definimos la plantilla para el número de página.
             $formato = utf8_decode('Convocatoria Becas de Investigación (Mocovi)                 '.date('d/m/Y h:i:s a').'     Página {PAGENUM} de {TOTALPAGENUM} ');
-            $pdf->ezStartPageNumbers(400, 20, 8, 'left', $formato, 1); //utf8_d_seguro($formato)
+            $pdf->ezStartPageNumbers(500, 20, 8, 'left', $formato, 1); //utf8_d_seguro($formato)
             //Configuración de Título.
             $salida->titulo(utf8_decode(''));   
             //$pdf->setLineStyle(1);
